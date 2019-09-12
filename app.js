07515709e9731ad50bf99063c8271dc2f249df0e
blaeugh <50417843+blaeugh@users.noreply.github.com>
2019-09-12 11:29:34 -0400

    quiz 2

---
+++ app.js
@@ -0,0 +1,10 @@
+let johnAverage = ((89 + 120 + 103) / 2);
+let mikeAverage = ((116 + 94 + 123) / 2);
+
+if (johnAverage > mikeAverage) {
+    console.log(`johns team good`);
+} else if (mikeAverage > johnAverage){
+    console.log(`mikes team good`);
+} else {
+    console.log(`both teams suck`);
+}
